561b9936e0c375411ab6e5672d46c10bd9616b26
Josef Butts <jbutts@utexas.edu>
2017-06-12 15:27:19 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,25 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+
+
+const sortedRobots = robots.map(function(robot) {
+if (knownDecepticons.includes(robot.name)) {
+  return Object.assign({}, robot, {
+    alliance: 'decepticon',
+
+  });
+} else {
+  return Object.assign({}, robot, {
+    alliance: 'autobot',
+
+  });
+}
+
+
+})
+
+//WHITE stripes
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +40,10 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+const coloredZebraStripes = zebraStripes.map((zebra, index)=>{
+  const isEven = (index%2)===0;
+  return Object.assign({}, zebra, {
+    color: isEven ? 'black' : 'white'
+  });
+});
