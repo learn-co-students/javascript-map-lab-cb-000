8b20c72f60bdb0407d7a334673f1a9fdbb5b04d3
john vascsinec <pensteeler@gmail.com>
2017-09-03 03:15:54 +0000

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,47 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+/*
+function map(collection, callback) {
+  const result = [];
+
+  for (let i = 0; i < collection.length; i++) {
+    const element = collection[i];
+    result.push(callback(element, i, collection));
+  }
+
+  return result;
+}
+*/
+/*
+const transformedAutobots = map(autobots, function (autobot) {
+  return Object.assign({}, autobot, {
+    strength: autobot.strength * 2,
+    isTransformed: true,
+  });
+});
+*/
+
+//const transformedAutobotsWithMap = autobots.map(function (autobot) {
+const sortedRobots = robots.map(function (robot) {
+var is_decepticon;
+
+    if (knownDecepticons.includes(robot.name)) {
+      is_decepticon = 'decepticon';
+    }
+    else {
+      is_decepticon = 'autobot';
+    }
+    return Object.assign({}, robot, {
+      alliance: is_decepticon
+  });
+});
+
+console.log(sortedRobots);
+
+const coloredZebraStripes = zebraStripes.map( function(zebraStripe, index) {
+  return Object.assign({}, zebraStripe, {
+    color: (index%2 == 0) ? 'black' : 'white'
+  });
+});
