648f97c3d91b31576a28d64b423be3ad5f3ef6cd
Cesar Castillo <cesarcast1llo@yahoo.com>
2017-11-08 00:29:34 +0000

    Done.

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ const robots = [
   { name: 'Ironhide', alliance: null },
   { name: 'Ratchet', alliance: null }
 ];
+//made a variable for the value of mapped 'robots' array
+var sortedRobots = robots.map(function(robot) {
+//running robot array to see if they match robot names with decepticon names..
+  return Object.assign({}, robot, {
+    name: robot.name,
+    alliance: (knownDecepticons.includes(robot.name)) ? 'decepticon' : 'autobot'
+//`Array.prototype.includes`, to check if an element exists in array
+//its asking if theres any names in knownDecepticons array that match with robot array
+  });
+});
 
 const zebraStripes = [
   { width: 9.12, color: null },
@@ -21,3 +31,16 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+//making variable..
+var coloredZebraStripes =
+//mapping zebraStripes array
+zebraStripes.map(function(zebraStripes, i){
+//.map() takes more than one parameter^^
+//returning original objects in zebraStripes array
+  return Object.assign({}, zebraStripes, {
+//if the stripes index is even, its black
+//if stripe index is odd, its white
+    color: (i % 2 === 0) ? 'black' : 'white'
+  });
+});
