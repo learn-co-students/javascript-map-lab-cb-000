73ceeb81002cf07c45e6197b3e96b081a4a6991a
theboymo <billfero@gmail.com>
2018-03-24 15:04:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const knownDecepticons = ['Megatron', 'Skywarp', 'Laserbeak', 'Barricade'];
 
 const robots = [
@@ -11,6 +13,17 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+var sortedRobots = robots.map((robot, i, robots)=>{
+  if (knownDecepticons.includes(robot.name)){
+    return Object.assign({}, robot, {alliance: 'decepticon'})
+  } else {
+    return Object.assign({}, robot, {alliance: 'autobot'})
+  }
+})
+
+
+//////////////////////////////////////////////////////
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +34,11 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+var coloredZebraStripes = zebraStripes.map((stripe, i, array)=>{
+  if(i % 2 === 0){
+    return Object.assign({}, stripe, {color: 'black'})
+  } else {
+    return Object.assign({}, stripe, {color: 'white'})
+  }
+})
