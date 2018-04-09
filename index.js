f9f4865acfe7acb96e5f75389b1be34ead5ad7d0
Chris Nunez <cenunez47@gmail.com>
2018-04-09 00:20:21 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+function allegiance(robot) {
+  if (!knownDecepticons.includes(robot.name)) {
+    return Object.assign({}, robot, {alliance: 'autobot'});
+  }
+  else {
+    return Object.assign({}, robot, {alliance: 'decepticon'});
+  }
+}
+
+const sortedRobots = robots.map(allegiance);
+
+
+////////////
+// ZEBRAS //
+////////////
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +37,14 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+function colorPicker(zebra) {
+  if (Math.floor(zebra.width % 2) === 0 && zebra.width >= 1 || zebra.width > Math.floor(zebra.width) + 0.7) {
+    return Object.assign({}, zebra, { color: 'black'});
+  }
+  else {
+    return Object.assign({}, zebra, {color: 'white'});
+  }
+}
+
+const coloredZebraStripes = zebraStripes.map(colorPicker);
