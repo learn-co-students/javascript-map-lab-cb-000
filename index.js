de4804b9914789b1c5c569300e392c7bff76a223
Joe Corwin <joe.corwin@gmail.com>
2019-02-21 01:45:06 +0000

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,32 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+
+const sortedRobots = robots.map(function(robot) {
+  if (knownDecepticons.includes(robot.name)) {
+    return Object.assign({}, robot, {
+      alliance: 'decepticon'
+    })
+  } else {
+    return Object.assign({}, robot, {
+      alliance: 'autobot'
+    })
+  }
+})
+
+
+var coloredZebraStripes = zebraStripes.map(function (zebra, idx) {
+  if (idx % 2) { var stripe = 'black';} else { var stripe = 'white'; }
+  return Object.assign({}, zebra, { color: stripe });
+})
+
+
+
+
+// Create a new variable called coloredZebraStripes;
+// The value of this variable is the mapped zebraStripes array;
+// Return each stripe object with the stripe filled in with either a 'black' or 'white' string.
+// A stripe is black when the stripe's index is even, and it's white if a stripe's index is odd.
+// (Hint: the callback we pass to .map() takes more than one parameter)
+// The original objects in the zebraStripes array must not be modified, so be sure to use Object.assign()
